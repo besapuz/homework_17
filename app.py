fbae4f115e24f732ec0efb457c4b9b46123c6f98
besapuz <aleksandzarinskij18772@gmail.com>
2022-06-13 15:42:31 +0300

    наладил работу json

--- app.py
+++ app.py
@@ -1,12 +1,71 @@
-from flask import Flask
+import json
+
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+
+from models import *
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///mybase.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JSON_AS_ASCII'] = False
 db = SQLAlchemy(app)
 
-app.route("/")
+
+@app.route('/users', methods=['GET', 'POST'])
+def users():
+    if request.method == 'GET':
+        user_data = User.query.all()
+        return json.dumps([user.to_dict() for user in user_data])
+
+
+@app.route('/users/<int:user_id>', methods=['GET', 'PUT', 'DELETE'])
+def one_user(user_id):
+    if request.method == 'GET':
+        user = User.query.get(user_id)
+        if user is None:
+            return "Не найден пользователь"
+        else:
+            return jsonify(user.to_dict())
+
+
+@app.route('/orders', methods=['GET', 'POST'])
+def orders():
+    if request.method == 'GET':
+        res = []
+        for order in Order.query.all():
+            res.append(order.to_dict_order())
+        return jsonify(res)
+
+
+@app.route('/orders/<int:order_id>', methods=['GET', 'PUT', 'DELETE'])
+def one_order(order_id):
+    if request.method == 'GET':
+        order = Order.query.get(order_id)
+        if order is None:
+            return "Нет приказа"
+        else:
+            return jsonify(order.to_dict_order())
+
+
+@app.route('/offers', methods=['GET', 'POST'])
+def offers():
+    if request.method == 'GET':
+        res = []
+        for offer in Offer.query.all():
+            res.append(offer.to_dict_offer())
+        return jsonify(res)
+
+
+@app.route('/offers/<int:offer_id>', methods=['GET', 'PUT', 'DELETE'])
+def one_offer(offer_id):
+    if request.method == 'GET':
+        offer = Offer.query.get(offer_id)
+        if offer is None:
+            return "Нет предложения"
+        else:
+            return jsonify(offer.to_dict_offer())
+
 
 if __name__ == '__main__':
     app.run(port=80)
--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 from app import db
 
+# from pprint import pprint as pp
+
 class User(db.Model):
     __tsblename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
@@ -9,6 +11,17 @@ class User(db.Model):
     email = db.Column(db.String(100))
     role = db.Column(db.String(100))
     phone = db.Column(db.String(100))
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "age": self.age,
+            "email": self.email,
+            "role": self.role,
+            "phone": self.phone
+        }
 
 
 class Order(db.Model):
@@ -23,9 +36,32 @@ class Order(db.Model):
     customer_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     executor_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
+    def to_dict_order(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "start_date": self.start_date,
+            "end_date": self.end_date,
+            "address": self.address,
+            "price": self.price,
+            "customer_id": self.customer_id,
+            "executor_id": self.executor_id
+        }
+
 
 class Offer(db.Model):
     __tablename__ = 'offer'
     id = db.Column(db.Integer, primary_key=True)
     order_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     executor_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def to_dict_offer(self):
+        return {
+            "id": self.id,
+            "order_id": self.order_id,
+            "executor_id": self.executor_id
+        }
+
+# rut = Offer()
+# pp(rut.to_dict())
